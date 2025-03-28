a243df8dd7ab5e9316107cc41c16fd3f96fa7b23
Jannis Zbinden <jannis.zbinden@proton.me>
2025-03-28 14:17:14 +0100

    fixed config tests

--- tests/test_Graph.py
+++ tests/test_Graph.py
@@ -13,6 +13,7 @@ class TestGraph:
     @pytest.fixture(scope="class", autouse=True)
     def setup_configurator(self, tmp_path_factory):
         config_data = {
+            "communication": {"device": "/dev/ttyAMA1", "baud": 9600},
             "angles": {
                 "X": {"S": 0.0},
                 "S": {"G": 30.0, "F": 60.0, "X": 180.0, "H": 300.0},
@@ -23,7 +24,7 @@ class TestGraph:
                 "A": {"B": 60.0, "I": 120.0, "H": 180.0},
                 "C": {"F": 180.0, "G": 210.0, "I": 240.0, "B": 300.0},
                 "B": {"C": 120.0, "I": 180.0, "A": 240.0},
-            }
+            },
         }
         config_file = tmp_path_factory.mktemp("config") / "config.json"
         with config_file.open("w") as f:
