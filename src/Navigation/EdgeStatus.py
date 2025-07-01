df42268faa499573a761ec16c887e6ebeb14e458
Jannis Zbinden <jannis.zbinden@proton.me>
2025-07-01 18:40:12 +0200

    updated edge weights

--- src/Navigation/EdgeStatus.py
+++ src/Navigation/EdgeStatus.py
@@ -1,11 +1,11 @@
 from enum import Enum
 
 class EdgeStatus(int, Enum):
-    UNKNOWN = 140
-    OBSTRUCTED = 130
+    UNKNOWN = 1000
+    OBSTRUCTED = 230
     FREE = 100
     MISSING = 9999999
     # POTENTIALLY means that the status was set based on information from the object detection (camera) which is not 100% reliable
-    POTENTIALLY_OBSTRUCTED = 120
+    POTENTIALLY_OBSTRUCTED = 230
     POTENTIALLY_FREE = 110
     POTENTIALLY_MISSING = 9999998
